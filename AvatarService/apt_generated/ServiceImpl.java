f2a6d80c19fe4cadb1e3be4ccc7c1477ee00d8be
Jonas <jonas_biedermann@hotmail.com>
2017-05-27 12:25:52 +0200

    All generated files now alphabetical for less commits

--- AvatarService/apt_generated/ServiceImpl.java
+++ AvatarService/apt_generated/ServiceImpl.java
@@ -17,21 +17,21 @@ public class ServiceImpl {
 	
 			AvatarService service = new AvatarService();
 	
-			peer.listen("/current/set", (Request request) -> service.setCurrentAvatar(context, request));
-peer.listen("/get", (Request request) -> service.setAvatar(context, request));
-peer.listen("/delete", (Request request) -> service.deleteAvatar(context, request));
-peer.listen("/current/get", (Request request) -> service.getCurrentAvatar(context, request));
-peer.listen("/persist", (Request request) -> service.persistAvatar(context, request));
-peer.listen("/current/update", (Request request) -> service.updateAvatar(context, request));
-peer.listen("/land", (Request request) -> service.land(context, request));
-peer.listen("/all", (Request request) -> service.getAllAvatars(context, request));
-peer.listen("/create", (Request request) -> service.createAvatar(context, request));
-peer.listen("/credits/add", (Request request) -> service.addCredits(context, request));
-peer.listen("/credits/balance", (Request request) -> service.balanceCredits(context, request));
-peer.listen("/credits/remove", (Request request) -> service.removeCredits(context, request));
-peer.listen("/takeoff", (Request request) -> service.takeoff(context, request));
-peer.listen("/current/name/get", (Request request) -> service.getCurrentAvatarName(context, request));
+			peer.listen("/credits/add", (Request request) -> service.addCredits(context, request));
 peer.listen("/reputation/add", (Request request) -> service.addReputation(context, request));
+peer.listen("/credits/balance", (Request request) -> service.balanceCredits(context, request));
+peer.listen("/create", (Request request) -> service.createAvatar(context, request));
+peer.listen("/delete", (Request request) -> service.deleteAvatar(context, request));
+peer.listen("/all", (Request request) -> service.getAllAvatars(context, request));
+peer.listen("/current/get", (Request request) -> service.getCurrentAvatar(context, request));
+peer.listen("/current/name/get", (Request request) -> service.getCurrentAvatarName(context, request));
+peer.listen("/land", (Request request) -> service.land(context, request));
+peer.listen("/persist", (Request request) -> service.persistAvatar(context, request));
+peer.listen("/credits/remove", (Request request) -> service.removeCredits(context, request));
+peer.listen("/get", (Request request) -> service.setAvatar(context, request));
+peer.listen("/current/set", (Request request) -> service.setCurrentAvatar(context, request));
+peer.listen("/takeoff", (Request request) -> service.takeoff(context, request));
+peer.listen("/current/update", (Request request) -> service.updateAvatar(context, request));
 
 			
 			System.out.println("AvatarService started...");
