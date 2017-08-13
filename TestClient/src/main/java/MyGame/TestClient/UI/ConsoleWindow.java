034594ef56a4f947d334dd162522e6d54d7f4188
Jonas <jonas_biedermann@hotmail.com>
2017-08-13 16:59:49 +0200

    Updated Example Game to all Requirements. It is basically done now...

---
+++ TestClient/src/main/java/MyGame/TestClient/UI/ConsoleWindow.java
@@ -0,0 +1,40 @@
+package MyGame.TestClient.UI;
+
+import java.util.Arrays;
+
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.gui2.ActionListBox;
+import com.googlecode.lanterna.gui2.BasicWindow;
+import com.googlecode.lanterna.gui2.Borders;
+import com.googlecode.lanterna.gui2.Window;
+
+public class ConsoleWindow extends BasicWindow {
+	
+	private ActionListBox actionListBox;
+
+	private int terminalWidth = 40;
+	private int terminalHeight = 10; 
+	
+	int horizontalMargin = 1;
+	int verticalMargin = 1;
+	
+	public ConsoleWindow() {
+		
+		actionListBox = new ActionListBox();
+		setComponent(actionListBox.withBorder(Borders.singleLine("Information")));
+		
+		setHints(Arrays.asList(Window.Hint.FIXED_POSITION, Window.Hint.FIXED_SIZE, Window.Hint.NO_DECORATIONS));
+		setSize(new TerminalSize(terminalWidth, terminalHeight));
+	}
+	
+	public void print(String msg) {
+		actionListBox.addItem(msg, ()->{});
+		
+		actionListBox.setSelectedIndex(actionListBox.getItemCount() - verticalMargin);
+	}
+
+	public void refreshLayout(TerminalSize terminalSize) {
+		setPosition(new TerminalPosition(horizontalMargin, terminalSize.getRows() - terminalHeight - verticalMargin));
+	}
+}
---
+++ VoteService/src/main/java/MyGame/VoteService/VoteService.java
@@ -0,0 +1,88 @@
+package MyGame.VoteService;
+
+import micronet.annotation.MessageListener;
+import micronet.annotation.MessageService;
+import micronet.annotation.OnStart;
+import micronet.annotation.OnStop;
+import micronet.network.Context;
+import micronet.network.NetworkConstants;
+import micronet.network.Request;
+import micronet.network.Response;
+import micronet.network.StatusCode;
+import micronet.serialization.Serialization;
+
+@MessageService(uri = "mn://vote")
+public class VoteService {
+
+	private RoundInfo currentRoundInfo;
+
+	VoteStore votes = new VoteStore();
+	
+	public static void main(String[] args) {
+//		votes.add(42,  314124);
+//		votes.add(43,  6);
+//		votes.add(44,  87);
+//		votes.add(45,  43);
+//		
+//		Map<Integer, Integer> all = votes.all();
+//		System.out.println(all);
+//		
+//		votes.clear();
+//		all = votes.all();
+//		System.out.println(all);
+//		
+//		votes.add(42,  314124);
+//		votes.add(43,  6);
+//		votes.add(44,  87);
+//		votes.add(45,  43);
+//		
+//		all = votes.all();
+//		System.out.println(all); 
+	}
+	
+	@OnStart
+	public void onStart(Context context) {
+		context.getAdvisory().listen(Event.RoundStart.toString(), event ->{ 
+			currentRoundInfo = Serialization.deserialize(event, RoundInfo.class);
+		});
+		context.getAdvisory().listen(Event.RoundEnd.toString(), event ->{ 
+			currentRoundInfo = null;
+		});
+	}
+
+	@OnStop
+	public void onStop(Context context) {
+	}
+	
+	@MessageListener(uri="/clear")
+	public Response clear(Context context, Request request) {
+		votes.clear();
+		return new Response(StatusCode.OK);
+	}
+	
+	@MessageListener(uri="/put")
+	public Response vote(Context context, Request request) {
+		int userID = request.getParameters().getInt(NetworkConstants.USER_ID);
+
+		if (votes.contains(userID))
+			return new Response(StatusCode.FORBIDDEN, "You already voted");
+		
+		Integer vote = Integer.parseInt(request.getData());
+		votes.add(userID, vote);
+		
+		int score = 101 - Math.abs(currentRoundInfo.getVoteValue() - vote);
+		score = score * score;
+
+		Request addScoreRequest = new Request(Integer.toString(score));
+		addScoreRequest.getParameters().set(NetworkConstants.USER_ID, userID);
+		context.sendRequest("mn://player/score/add", addScoreRequest);
+		
+		VoteResult result = new VoteResult();
+		result.setMessage("Thank you for playing!");
+		result.setRealValue(currentRoundInfo.getVoteValue());
+		result.setScore(score);
+		
+		return new Response(StatusCode.OK, Serialization.serialize(result));
+	}
+
+}
