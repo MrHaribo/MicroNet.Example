60ffdbc676ee5e3eb89b72b508024f0d71bceb79
Jonas <jonas_biedermann@hotmail.com>
2017-08-14 19:00:52 +0200

    Contributions of Examples: Round/VoteService + TestClient missing

--- PlayerService/src/main/java/MyGame/PlayerService/PlayerStore.java
+++ PlayerService/src/main/java/MyGame/PlayerService/PlayerStore.java
@@ -13,6 +13,7 @@ import com.couchbase.client.java.query.N1qlQuery;
 import com.couchbase.client.java.query.N1qlQueryResult;
 import com.couchbase.client.java.query.N1qlQueryRow;
 
+import MyGame.PlayerService.Player;
 import micronet.model.Entity;
 import micronet.serialization.Serialization;
 
